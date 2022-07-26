6f1c89eccd183628b262f9d22061f4065f2fa07f
Garrett Mould <garrettmould@gmail.com>
2022-07-26 11:10:51 +0700

    footer fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -15,7 +15,7 @@ const Footer = (props) => {
     <div id="footer" className={classes.padding}>
       <div className="w-100 d-flex flex-column align-items-center">
         <img className={classes.mainlogo} src={mainlogo}></img>
-        <div className={classes.footerText}>"+0824675798"</div>
+        <div className={classes.footerText}>+0824675798</div>
         <div className={classes.footerText}>snapshothanoi@gmail.com</div>
         <div className={classes.iconRow}>
           <a href="#">
